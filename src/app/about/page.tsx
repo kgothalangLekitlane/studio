459676d73c7de76e0011e49df5321e7f50579382
Kgothalang Isaac Lekitlane <isaacspeedy101@gmail.com>
2025-05-04 14:57:35 +0200

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Globe, Feather, Camera } from 'lucide-react'; // Changed icons
+import { Globe, Feather, Camera } from 'lucide-react'; // 
 
 export default function AboutPage() {
   return (
